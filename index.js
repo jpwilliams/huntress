27a769d17238f42bdf10bb56eb7d3448b3787df6
Jack Williams <jpwilliamsphotography@gmail.com>
2018-02-01 16:23:32 +0000

    feat(react): add HOC to pass props when designated state changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { Component, createElement } = require('react')
 const { EagerEmitter } = require('./lib/EagerEmitter')
 const { RecursiveProxy } = require('./lib/RecursiveProxy')
 
@@ -11,6 +12,52 @@ exports.createWatcher = function createWatcher (state = {}) {
 }
 
 exports.Watcher = exports.createWatcher()
+
+exports.withWatcher = (paths, watcher) => {
+  const w = watcher || exports.Watcher
+
+  return WrappedComponent => class extends Component {
+    constructor (props) {
+      super(props)
+      this.fnMap = {}
+    }
+
+    componentDidMount () {
+      const propKeys = Object.keys(paths)
+
+      propKeys.forEach((key) => {
+        const path = paths[key]
+        const fn = this.createStateUpdater(key)
+        this.fnMap[path] = fn
+        w.track(path, fn)
+      })
+    }
+
+    componentWillUnmount () {
+      const propKeys = Object.keys(paths)
+
+      propKeys.forEach((key) => {
+        const path = paths[key]
+        const fn = this.fnMap[path]
+        w.stopTracking(path, fn)
+      })
+    }
+
+    createStateUpdater (key) {
+      return (value) => {
+        this.setState({[key]: value})
+      }
+    }
+
+    render () {
+      return createElement(
+        WrappedComponent,
+        Object.assign({}, this.state, this.props),
+        this.props.children
+      )
+    }
+  }
+}
 
 function track (emitter, bits, callback) {
   emitter.on(genPath(bits), callback)
